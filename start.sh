30c09d89c678feac8e5f18821ced8eb6fc4987b5
Munge Ryan <mungeryan@gmail.com>
2020-10-23 09:51:02 +0300

    exporting the mail username and password

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 export MOVIE_API_KEY=e7566dd2112691f089d11fe091226a2d
 export SECRET_KEY=Flask WTF Secret Key
 
+export MAIL_USERNAME = rgmunge@gmail.com
+export MAIL_PASSWORD = slowwhine
+
 python manage.py server
